b5ae275f9a36467f9a43dddad93132f6b7df484f
Noemie <noemie.clay@hotmail.fr>
2017-01-17 14:10:26 +0100

    Pagination

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
       header("Location: index.php");
     }
 }
+
+
 ?>
 <div class="row">
     <form method="post" action="message.php">
@@ -34,7 +36,17 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
 </div>
 
 <?php
-$query = 'SELECT * FROM messages';
+
+// Pagination
+$index = 0;
+$mpp = 4;
+if(isset($_GET['p']) && !empty($_GET['p'])){
+  $index = $_GET['p'];
+  $index = ($index - 1)* $mpp;
+}
+$query = 'SELECT * FROM messages LIMIT '.$index.','.$mpp.'';
+echo $query;
+//$query = 'SELECT * FROM messages';
 $stmt = $pdo->query($query);
 
 while ($data = $stmt->fetch()) {
@@ -43,10 +55,10 @@ while ($data = $stmt->fetch()) {
   	<blockquote>
   		<?= $data['contenu'] ?>
       <div class="col-sm-2">
-          <?php echo "<a href='index.php?id=" .$data['id']. "'><button type='button' class='btn btn-warning'>Modifier</button></a>" ?>
+          <?php echo "<a href='index.php?id=" .$data['id']."&p=".$page."'><button type='button' class='btn btn-warning'>Modifier</button></a>" ?>
       </div>
       <div class="col-sm-2">
-          <?php echo "<a href='suppression.php?id=" .$data['id']. "'><button type='button' class='btn btn-danger'>Supprimer</button></a>" ?>
+          <?php echo "<a href='suppression.php?id=" .$data['id']."&p=".$page."'><button type='button' class='btn btn-danger'>Supprimer</button></a>" ?>
       </div>
       <div class="col-sm-12">
           <?= "Ajouté le ".$data['date'] ?>
@@ -70,4 +82,38 @@ while ($data = $stmt->fetch()) {
     <?php
   }
 }
- include('includes/bas.inc.php'); ?>
+?>
+<?php
+
+$requete = 'SELECT COUNT(*) as total_messages FROM messages';
+$prep = $pdo->query($requete);
+$data = $prep->fetch();
+$nombre_message = $data['total_messages'];
+
+$nb_pages = ($nombre_message) ? ceil($nombre_message/$mpp) : 1;
+
+
+?>
+<nav aria-label="Page navigation">
+  <ul class="pagination">
+    <li>
+      <a href="#" aria-label="Previous">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <?php for ($i=1; $i < $nb_pages+1; $i++) {
+      ?>
+    <li>  <?php echo "<a href='index.php?p=$i'>$i</a>" ?></li>
+    <?php
+    }
+     ?>
+
+    <li>
+      <a href="#" aria-label="Next">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+
+ <?php include('includes/bas.inc.php'); ?>
