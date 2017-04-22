0f211b3ce5828742e3e96ef00bf7ac412db90e19
Noemie <noemie.clay@hotmail.fr>
2017-04-22 17:56:48 +0200

    Add CDN Bootstrap and JQuery

--- includes/bas.inc.php
+++ includes/bas.inc.php
@@ -45,11 +45,9 @@
         </a>
     </div>
 
-    <!-- jQuery -->
-    <script src="vendor/jquery/jquery.min.js"></script>
+    
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
     <!-- Plugin JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
